ed8487b67e899c94222c09c62b4e07aae5c9f7aa
Manoj Acharya <manoj.acharya@sri.com>
2023-09-15 09:14:04 -0700

    generator

---
+++ metafile_generator.py
@@ -0,0 +1,48 @@
+#Github link
+url="https://github.com/manoja328/trojai-example"
+#commit id
+
+import subprocess
+import re
+process = subprocess.Popen(["git", "ls-remote", url], stdout=subprocess.PIPE)
+stdout, stderr = process.communicate()
+sha = re.split(r'\t+', stdout.decode('ascii'))[0]
+print(sha)
+
+
+template={}
+template["nlayers_1"] =  {
+    "description": "The hidden number of layers for training.",
+    "type": "integer",
+    "minimum": 1,
+    "maximum": 3,
+    "suggested_minimum": 2,
+    "suggested_maximum": 3
+}
+
+
+meta_schema= {"$schema": "http://json-schema.org/draft-07/schema#",
+              "title": "SRI Trinity Framework",
+              "technique": "SRI value , action gradient based pipeline",
+              "technique_description": "We are looking at action/value gradient, values and the final rewards",
+              "technique_changes": "Some bug fixes on inferencing and loss calculation. Small changes to classifier design.",
+              "technique_type": ["Trigger Inversion", "Jacobian Inspection"],
+              "commit_id": sha,
+              "repo_name": url,
+              "required": [],
+              "additionalProperties": False,
+              "type": "object",
+              'properties': template}
+
+params = {}
+for k in template:
+    params[k] = 2
+
+import json
+with open('metaparameters_schema.json','w') as f:
+    json.dump(meta_schema,f)
+
+with open('metaparameters.json','w') as f:
+    json.dump(params,f)
+
+print("Metaparameters generated and saved.....")
